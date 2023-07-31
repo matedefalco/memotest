86ccbd9834d24d553cacc1b6b9ddc6cfcf2e211d
Suka <mateodefalco1@gmail.com>
2023-07-31 16:13:03 -0300

    Define state changes & img rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css"
+import { useState } from "react"
 
 const images = [
 	"https://icongr.am/devicon/angularjs-original.svg?size=128&color=currentColor",
@@ -12,8 +13,13 @@ const images = [
 	"https://icongr.am/devicon/javascript-original.svg?size=128&color=currentColor",
 	"https://icongr.am/devicon/python-original.svg?size=128&color=currentColor",
 ]
+	.flatMap((image) => [`a|${image}`, `b|${image}`])
+	.sort(() => Math.random() - 0.5)
 
 export default function App() {
+	const [guessed, setGuessed] = useState([])
+	const [selected, setSelected] = useState([])
+
 	return (
 		<>
 			<h1 style={{ fontSize: "50px", marginBottom: 8 }}>Memotest</h1>
@@ -24,19 +30,32 @@ export default function App() {
 					gap: 24,
 				}}
 			>
-				{images.map((image, index) => (
-					<li
-						style={{
-							curor: "pointer",
-							padding: 12,
-							border: "1px solid #666",
-							borderRadius: 12,
-						}}
-						key={index}
-					>
-						<img src={image} alt="icon" />
-					</li>
-				))}
+				{images.map((image, index) => {
+					const [, url] = image.split("|")
+					return (
+						<li
+							onClick={() => setSelected((selected) => selected.concat(image))}
+							style={{
+								curor: "pointer",
+								padding: 12,
+								border: "1px solid #666",
+								borderRadius: 12,
+							}}
+							key={index}
+						>
+							{selected.includes(image) || guessed.includes(image) ? (
+								<img src={url} alt="icon" />
+							) : (
+								<img
+									src={
+										"https://icongr.am/fontawesome/bomb.svg?size=128&color=currentColor"
+									}
+									alt="icon"
+								/>
+							)}
+						</li>
+					)
+				})}
 			</ul>
 		</>
 	)
